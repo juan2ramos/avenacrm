e485e7a98700785dc5ca2fadfc7a8199879ddd9b
juan2ramos <juan2ramos@gmail.com>
2018-01-16 14:28:11 -0500

    adreess

--- database/factories/ModelsPointFactory.php
+++ database/factories/ModelsPointFactory.php
@@ -1,10 +1,10 @@
 <?php
 
 use Faker\Generator as Faker;
-use Faker\Provider\es_Es\Address;
+
 
 $factory->define(App\Models\Point::class, function (Faker $faker) {
-    $faker->addProvider(new Address($faker));
+
     return [
         'name' => $faker->name,
         'address' => $faker->address,

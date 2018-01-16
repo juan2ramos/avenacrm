1ec483a2ab14b2f271fd7b4962491d9728bfc239
juan2ramos <juan2ramos@gmail.com>
2018-01-16 14:49:09 -0500

    adreess

--- app/Models/ProductPointBase.php
+++ app/Models/ProductPointBase.php
@@ -7,15 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class ProductPointBase extends Model
 {
     protected $table = 'point_product_base';
+
     protected $fillable = ['sale_value'];
 
     static function valueFormat($values)
     {
-        foreach ($values as $key => $value) {
-            $values[$key]['sale_value'] = intval(preg_replace('/[^0-9]+/', '', substr($value['sale_value'], 0, -2)), 10);;
+        if ($values) {
+
+            foreach ($values as $key => $value) {
+                $values[$key]['sale_value'] = intval(preg_replace('/[^0-9]+/', '', substr($value['sale_value'], 0, -2)), 10);;
+            }
         }
+
         return $values;
     }
-
-
 }

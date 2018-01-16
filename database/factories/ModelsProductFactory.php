5098baffd971fea2589f56e80a49cb8e475a8ffe
juan2ramos <juan2ramos@gmail.com>
2018-01-16 14:39:21 -0500

    adreess

--- database/factories/ModelsProductFactory.php
+++ database/factories/ModelsProductFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 use Faker\Provider\es_Es\Text;
 
 $factory->define(App\Models\Product::class, function (Faker $faker) {
-    $faker->addProvider(new Text($faker));
+
     return [
         'name' => $faker->name,
         'sale_value' => $faker->numberBetween(1000,5000),
